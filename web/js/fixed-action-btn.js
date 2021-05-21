cae62afd5ae74892858fa9a25c086a4d037355d4
Damien Minenna <damien.minenna+bitbucket@gmail.com>
2021-05-21 22:40:27 +0200

    Update website

--- web/js/fixed-action-btn.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function(){
-  $('.fixed-action-btn').floatingActionButton();
-});
--- web/js/scrollspy.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.scrollspy');
-    var instances = M.ScrollSpy.init(elems, {
-        'scrollOffset': 50
-    });
-});
